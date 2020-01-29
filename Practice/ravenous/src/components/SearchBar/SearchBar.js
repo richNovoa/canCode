47a392699982bb82706b6febd83d56958363e8cb
richNovoa <rich@cross.team>
2020-01-28 22:25:47 -0500

    finished Part 1 Ravenous
    code is broken requires debug

--- Practice/ravenous/src/components/SearchBar/SearchBar.js
+++ Practice/ravenous/src/components/SearchBar/SearchBar.js
@@ -2,20 +2,29 @@ import React from "react";
 import "./SearchBar.css"
 
 const sortByOptions = {
-'Best Match': 'best_match';
-'Highest Rated': 'rating';
-'Most Reviewed': 'review_count';
+'Best Match': 'best_match',
+'Highest Rated': 'rating',
+'Most Reviewed': 'review_count',
 }
 
 class SearchBar extends React.Component {
-    render() {
-        renderSortByOptions() {
-            return Objects.keys(sortByOptions).map(sortByOption =>{
-                let sortByOption = sortByOptions[sortByOption];
-                return <li key="sortByOptions">{sortByOptions}</li>
-            });
-            )
-        }
-    }
+render() {
+    return (
+        <div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+    {this.renderSortByOptions()}
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input placeholder="Search Businesses" />
+    <input placeholder="Where?" />
+  </div>
+  <div className="SearchBar-submit">
+    <a>Let's Go</a>
+  </div>
+</div>
+    )
 }
 
+export default SearchBar;
