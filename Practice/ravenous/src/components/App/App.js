703d1c57567738afb8a70ceca688c3abc5cebb92
richNovoa <rich@cross.team>
2020-05-13 15:02:48 -0400

    test

--- Practice/ravenous/src/components/App/App.js
+++ Practice/ravenous/src/components/App/App.js
@@ -6,7 +6,7 @@ import BusinessList from '../BusinessList/BusinessList.css';
 class App extends React.Component {
   render() {
   return (
-    <div className="App">
+    <div className="App" id="App">
   <h1>ravenous</h1>
   <SearchBar />
   <BusinessList />
