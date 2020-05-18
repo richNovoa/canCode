14408f9470f23e086d60742c291cf9a11fa7525c
richNovoa <rich@cross.team>
2020-05-18 11:49:41 -0400

    .

--- Practice/ravenous/src/components/BusinessList/BusinessList.js
+++ Practice/ravenous/src/components/BusinessList/BusinessList.js
@@ -1,18 +1,20 @@
-import React from "react";
-import "./BusinessList.css";
-import Business from "../Business/Business";
+import React from 'react';
+import './BusinessList.css';
 
-class BusinessList extends React.Component{
-    render() {
-<div className="BusinessList">
-    <Business />
-    <Business />
-    <Business />
-    <Business />
-    <Business />
-    <Business />
-</div>
-    }
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
