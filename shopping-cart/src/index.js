c33d7447a0c689320ba6f8e0d0f89c6b2374c935
richNovoa <rich@cross.team>
2020-10-13 13:08:07 -0400

    connect graphCMS

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: "https://api-us-east-1.graphcms.com/v2/ckg83tfn3rm0n01z6gk2l77nn/master"
+})
 
 ReactDOM.render(
   <React.StrictMode>
