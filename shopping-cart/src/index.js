5273c64552caf233f82192f1e34a4ef7b96d6144
richNovoa <rich@cross.team>
2020-10-13 20:41:02 -0400

    Finished

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -8,16 +8,13 @@ import * as serviceWorker from './serviceWorker';
 
 const client = new ApolloClient({
   uri: "https://api-us-east-1.graphcms.com/v2/ckg83tfn3rm0n01z6gk2l77nn/master"
-})
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
